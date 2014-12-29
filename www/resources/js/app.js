4ed2be7eb27e69c20d67c749f64905d56ffe4875
Kyle A Anderson <kyleaa@gmail.com>
2014-12-29 14:58:03 -0500

    added angularjs files

---
+++ www/resources/js/app.js
@@ -0,0 +1,28 @@
+(function(){
+	var app = angular.module('queryTerminal',['ngRoute','validation.match','$http']);
+	
+	
+	app.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider
+	    .when("/settings", { templateUrl: 'resources/views/settings.html', controller: 'SettingsController' })
+	    .otherwise( {templateUrl: 'resources/views/dashboard.php' } );
+	}]);
+	
+	app.controller('SidebarController',function() {
+		this.items = [ { id:1, name:'connection1' }, { id:2, name:'connection2' } ];
+	});
+	app.controller('TopNavController',function() {
+		this.items = [  { label: 'Settings', href: '#/settings' }, { label: 'Sign Out', href: '/?signout=y' }];
+	});
+	
+	app.controller('SettingsController',function() {
+	
+	});
+	
+	app.controller('PasswordController',function() {
+	  this.setPassword = function(user) {
+	  
+	  }
+	});
+	
+})();
